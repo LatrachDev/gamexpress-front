fed29a068ee750a677f764c4e9834d26630aedd9
LatrachDev <m.latrach.youcode@gmail.com>
2025-04-08 10:40:14 +0100

    GXF-2 Configuration de Tailwind CSS & React Router

---
+++ src/components/header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='text-black font-bold'>
+      <h1>Header Component</h1>
+      <p>This is the Header. Route is working!</p>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/test.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center', backgroundColor: '#f0f0f0' }}>
+      <h1>test</h1>
+      <p>This is test. Route is working!</p>
+    </div>
+  )
+}
+
+export default Header
