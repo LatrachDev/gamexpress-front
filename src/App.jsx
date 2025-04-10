73e09b0076595e397bc9cde509c0f619eefc124a
LatrachDev <m.latrach.youcode@gmail.com>
2025-04-10 09:42:29 +0100

    GXF-3 implement the authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,48 @@
-import './App.css'
-import Main from './components/Main'
-import Header from './components/header'
-import Footer from './components/footer'
-import Test from './components/test'
-import { BrowserRouter , Routes , Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import ProtectedRoute from './components/ProtectedRoute';
+import AuthRoute from './components/AuthRoute'; 
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
+import Home from './pages/Home';
+import Unauthorized from './pages/Unauthorized';
+import Layout from './components/Layout';
 
 function App() {
-
   return (
-      <div className="App">
-      {
-        <>
-          <Main />
-          <BrowserRouter>
-
-            {/* <Header /> */}
-
-            <Routes>
-              <Route path="/" element={<Main />} />
-              <Route path="/header" element={<Header />} />
-              <Route path="/footer" element={<Footer />} />
-              <Route path='/test' element={<Test />} />
-            </Routes>
-
-            {/* <Footer /> */}
-
-          </BrowserRouter>
-        </>
-      }
-      </div>
-  )
+    <Router>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            
+          
+            <Route element={<AuthRoute />}>
+              <Route path="login" element={<Login />} />
+              <Route path="register" element={<Register />} />
+            </Route>
+            
+            <Route path="unauthorized" element={<Unauthorized />} />
+            
+            {/* Protected routes */}
+            <Route element={<ProtectedRoute roles={['product_manager', 'super_admin']} />}>
+              <Route path="dashboard" element={<Dashboard />} />
+            </Route>
+            
+            <Route element={<ProtectedRoute roles={['super_admin']} />}>
+              <Route path="categories" element={<div>Categories Management</div>} />
+            </Route>
+            
+            <Route element={<ProtectedRoute roles={['product_manager', 'super_admin']} />}>
+              <Route path="products" element={<div>Products Management</div>} />
+            </Route>
+          </Route>
+          <Route path="*" element={<div>404</div>} />
+        </Routes>
+      </AuthProvider>
+    </Router>
+  );
 }
 
-export default App
+export default App;
